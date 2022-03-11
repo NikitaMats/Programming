f78e44da6eaf39fbcf7050e5f0e34d4a5d83ddfa
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-11 14:43:14 +0700

    Добавленны первые классы из задачи 3

---
+++ Programming/Model/Film.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    internal class Film
+    {
+        public string Name;
+        public int DurationMin;
+        public int ReleaseYear;
+        public string Genre;
+        public double Rating;
+    }
+}
---
+++ Programming/Model/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    internal class Rectangle
+    {
+        public double length;
+        public double width;
+        public string color;
+    }
+}
