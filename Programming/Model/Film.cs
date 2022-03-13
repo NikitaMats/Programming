7893a7cc4189badde501abc66756a88ae69f6e0b
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-13 16:41:13 +0700

    Настроен класс Film

--- Programming/Model/Film.cs
+++ Programming/Model/Film.cs
@@ -6,12 +6,62 @@ using System.Threading.Tasks;
 
 namespace Programming.Model
 {
-    internal class Film
+    public class Film
     {
-        private string Name;
-        private int DurationMinute;
-        private int ReleaseYear;
-        private string Genre;
-        private double Rating;
+        private int _durationMinute;
+
+        private int _releaseYear;
+
+        private double _rating;
+
+        public Film()
+        { }
+
+        public Film(string name, int durationminute, int releaseyear,string genre,double rating)
+        {
+            Name = name;
+            DurationMinute = durationminute;
+            ReleaseYear = releaseyear;
+            Genre = genre;
+            Rating = rating;
+        }
+        public string Name { get; set; }
+        public int DurationMinute 
+        {
+            get
+            {
+                return _durationMinute;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _durationMinute = value;
+            }
+        }
+        public int ReleaseYear 
+        {
+            get
+            {
+                return _releaseYear;
+            }
+            set
+            {
+                if (value < 1900 && value > 2022) throw new ArgumentException();
+                _releaseYear = value;
+            }
+        }
+        public string Genre { get; set; }
+        public double Rating 
+        {
+            get
+            {
+                return _rating;
+            }
+            set
+            {
+                if (value < 0 && value > 10) throw new ArgumentException();
+                _rating = value;
+            }
+        }
     }
 }
