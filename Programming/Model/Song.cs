63a9e611abf92a7de9e2a78bde92ea52add062aa
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-13 17:15:32 +0700

    Настроен класс Song

--- Programming/Model/Song.cs
+++ Programming/Model/Song.cs
@@ -8,9 +8,43 @@ namespace Programming.Model
 {
     internal class Song
     {
-        private int Id;
-        private string Name;
-        private string Author;
-        private double Duration;
+        private int _id;
+        private int _durationMinute;
+        public Song()
+        { }
+
+        public Song(int id, string name, string autor, int duretionminute)
+        {
+            Id = id;
+            Name = name;
+            Autor = autor;
+            DuretionMinute = duretionminute;
+        }
+        public int Id 
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _id = value;
+            }
+        }
+        public string Name { get; set; }
+        public string Autor { get; set; }
+        public int DuretionMinute 
+        {
+            get
+            {
+                return _durationMinute;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _durationMinute = value;
+            }
+        }
     }
 }
