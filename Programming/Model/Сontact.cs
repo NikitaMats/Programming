2cbab684ae62b245c33b5c9b07867c55acbd79d2
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-13 16:42:04 +0700

    Убрано лишнее присваивание num

--- Programming/Model/Сontact.cs
+++ Programming/Model/Сontact.cs
@@ -29,7 +29,7 @@ namespace Programming.Model
             set 
             {
                 if (value.Length != 11) throw new ArgumentException();
-                if (int .TryParse(value, out int num)==false) throw new ArgumentException();
+                if (int .TryParse(value, out _)==false) throw new ArgumentException();
                 _number = value;
             }
         }
