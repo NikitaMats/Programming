88b686d3e92967e2bd6cd109acec88a88af9ba49
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-13 17:06:48 +0700

    Настроен класс Rectangle

--- Programming/Model/Rectangle.cs
+++ Programming/Model/Rectangle.cs
@@ -8,8 +8,42 @@ namespace Programming.Model
 {
     internal class Rectangle
     {
-        private double Length;
-        private double Width;
-        private string Color;
+        private double _length;
+        private double _width;
+
+        public Rectangle()
+        { }
+
+        public Rectangle(double lenght, double width, string color)
+        {
+            Length = lenght;
+            Width = width;
+            Color = color;
+        }
+        public double Length 
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _length = value;
+            }
+        }
+        public double Width 
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _width = value;
+            }
+        }
+        public string Color { get; set; }
     }
 }
