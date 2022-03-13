6451fb8685022b36408feac6fb250077c523bd46
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-03-13 17:24:46 +0700

    Настроил класс Subject

--- Programming/Model/Subject.cs
+++ Programming/Model/Subject.cs
@@ -8,8 +8,30 @@ namespace Programming.Model
 {
     internal class Subject
     {
-        private string Lesson;
-        private string Instructor;
-        private string Time;
+        private int _position;
+
+        public Subject()
+        { }
+
+        public Subject(string lesson, string instructor, int position)
+        {
+            Lesson = lesson;
+            Instructor = instructor;
+            Position = position;
+        }
+        public string Lesson { get; set; }
+        public string Instructor { get; set; }
+        public int Position 
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException();
+                _position = value;
+            }
+        }
     }
 }
