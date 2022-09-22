0c7bca8492a7c3d906a30acb3425b401086ef907
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-09-23 00:16:01 +0700

    Layout full Items

--- ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -36,6 +36,13 @@
             this.ItemsListBox = new System.Windows.Forms.ListBox();
             this.Label1 = new System.Windows.Forms.Label();
             this.CustomersTab = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.TabControl.SuspendLayout();
             this.ItemsTab.SuspendLayout();
             this.SuspendLayout();
@@ -54,6 +61,13 @@
             // 
             // ItemsTab
             // 
+            this.ItemsTab.Controls.Add(this.textBox3);
+            this.ItemsTab.Controls.Add(this.label5);
+            this.ItemsTab.Controls.Add(this.textBox2);
+            this.ItemsTab.Controls.Add(this.textBox1);
+            this.ItemsTab.Controls.Add(this.label4);
+            this.ItemsTab.Controls.Add(this.label3);
+            this.ItemsTab.Controls.Add(this.label2);
             this.ItemsTab.Controls.Add(this.DeleteItemButton);
             this.ItemsTab.Controls.Add(this.AddItemButtom);
             this.ItemsTab.Controls.Add(this.ItemsListBox);
@@ -69,17 +83,17 @@
             // 
             // DeleteItemButton
             // 
-            this.DeleteItemButton.Location = new System.Drawing.Point(121, 318);
+            this.DeleteItemButton.Location = new System.Drawing.Point(116, 318);
             this.DeleteItemButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DeleteItemButton.Name = "DeleteItemButton";
             this.DeleteItemButton.Size = new System.Drawing.Size(82, 26);
             this.DeleteItemButton.TabIndex = 3;
-            this.DeleteItemButton.Text = "button2";
+            this.DeleteItemButton.Text = "Delete";
             this.DeleteItemButton.UseVisualStyleBackColor = true;
             // 
             // AddItemButtom
             // 
-            this.AddItemButtom.Location = new System.Drawing.Point(9, 318);
+            this.AddItemButtom.Location = new System.Drawing.Point(15, 318);
             this.AddItemButtom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddItemButtom.Name = "AddItemButtom";
             this.AddItemButtom.Size = new System.Drawing.Size(82, 26);
@@ -117,6 +131,64 @@
             this.CustomersTab.Text = "Customers";
             this.CustomersTab.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Selected Item:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(218, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Cost:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(255, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(218, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Name:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(221, 102);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(332, 59);
+            this.textBox3.TabIndex = 10;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -143,6 +215,13 @@
         private System.Windows.Forms.Button AddItemButtom;
         private System.Windows.Forms.ListBox ItemsListBox;
         private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
