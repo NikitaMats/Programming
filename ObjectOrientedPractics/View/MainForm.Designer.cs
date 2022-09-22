3e8a9608af7c69975a7dc106c7cb71526ac6d8a9
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-09-23 00:02:54 +0700

    Layout part 1

--- ObjectOrientedPractics/View/MainForm.Designer.cs
+++ ObjectOrientedPractics/View/MainForm.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.ItemsTab = new System.Windows.Forms.TabPage();
+            this.CustomersTab = new System.Windows.Forms.TabPage();
+            this.TabControl.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.ItemsTab);
+            this.TabControl.Controls.Add(this.CustomersTab);
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(800, 450);
+            this.TabControl.TabIndex = 0;
+            // 
+            // ItemsTab
+            // 
+            this.ItemsTab.Location = new System.Drawing.Point(4, 25);
+            this.ItemsTab.Name = "ItemsTab";
+            this.ItemsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.ItemsTab.Size = new System.Drawing.Size(792, 421);
+            this.ItemsTab.TabIndex = 0;
+            this.ItemsTab.Text = "Items";
+            this.ItemsTab.UseVisualStyleBackColor = true;
+            // 
+            // CustomersTab
+            // 
+            this.CustomersTab.Location = new System.Drawing.Point(4, 25);
+            this.CustomersTab.Name = "CustomersTab";
+            this.CustomersTab.Padding = new System.Windows.Forms.Padding(3);
+            this.CustomersTab.Size = new System.Drawing.Size(792, 421);
+            this.CustomersTab.TabIndex = 1;
+            this.CustomersTab.Text = "Customers";
+            this.CustomersTab.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TabControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Object Oriented Practics";
+            this.TabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage ItemsTab;
+        private System.Windows.Forms.TabPage CustomersTab;
     }
 }
 
