85b5f806b2d73f4d859eaad7ec526b594697103a
Мацкевич Никита <nikitafromtomsk@gmail.com>
2022-09-23 01:14:38 +0700

    Items window logic

--- ObjectOrientedPractics/View/Tabs/ItemsTabControl.Designer.cs
+++ ObjectOrientedPractics/View/Tabs/ItemsTabControl.Designer.cs
@@ -45,121 +45,134 @@
             // 
             // ItemInfoTextBox
             // 
-            this.ItemInfoTextBox.Location = new System.Drawing.Point(220, 190);
+            this.ItemInfoTextBox.Location = new System.Drawing.Point(293, 234);
+            this.ItemInfoTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ItemInfoTextBox.Multiline = true;
             this.ItemInfoTextBox.Name = "ItemInfoTextBox";
-            this.ItemInfoTextBox.Size = new System.Drawing.Size(332, 123);
+            this.ItemInfoTextBox.Size = new System.Drawing.Size(441, 150);
             this.ItemInfoTextBox.TabIndex = 25;
+            this.ItemInfoTextBox.TextChanged += new System.EventHandler(this.ItemInfoTextBox_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(220, 174);
+            this.label6.Location = new System.Drawing.Point(293, 214);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.Size = new System.Drawing.Size(31, 16);
             this.label6.TabIndex = 24;
             this.label6.Text = "Info:";
             // 
             // ItemNameTextBox
             // 
-            this.ItemNameTextBox.Location = new System.Drawing.Point(220, 101);
+            this.ItemNameTextBox.Location = new System.Drawing.Point(293, 124);
+            this.ItemNameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ItemNameTextBox.Multiline = true;
             this.ItemNameTextBox.Name = "ItemNameTextBox";
-            this.ItemNameTextBox.Size = new System.Drawing.Size(332, 59);
+            this.ItemNameTextBox.Size = new System.Drawing.Size(441, 72);
             this.ItemNameTextBox.TabIndex = 23;
+            this.ItemNameTextBox.TextChanged += new System.EventHandler(this.ItemNameTextBox_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(217, 85);
+            this.label5.Location = new System.Drawing.Point(289, 105);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.Size = new System.Drawing.Size(47, 16);
             this.label5.TabIndex = 22;
             this.label5.Text = "Name:";
             // 
             // ItemIDTextBox
             // 
-            this.ItemIDTextBox.Location = new System.Drawing.Point(254, 30);
+            this.ItemIDTextBox.Location = new System.Drawing.Point(339, 37);
+            this.ItemIDTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ItemIDTextBox.Name = "ItemIDTextBox";
-            this.ItemIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemIDTextBox.Size = new System.Drawing.Size(132, 22);
             this.ItemIDTextBox.TabIndex = 21;
             // 
             // ItemCostTextBox
             // 
-            this.ItemCostTextBox.Location = new System.Drawing.Point(254, 56);
+            this.ItemCostTextBox.Location = new System.Drawing.Point(339, 69);
+            this.ItemCostTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ItemCostTextBox.Name = "ItemCostTextBox";
-            this.ItemCostTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ItemCostTextBox.Size = new System.Drawing.Size(132, 22);
             this.ItemCostTextBox.TabIndex = 20;
+            this.ItemCostTextBox.TextChanged += new System.EventHandler(this.ItemCostTextBox_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(217, 59);
+            this.label4.Location = new System.Drawing.Point(289, 73);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.Size = new System.Drawing.Size(37, 16);
             this.label4.TabIndex = 19;
             this.label4.Text = "Cost:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(217, 33);
+            this.label3.Location = new System.Drawing.Point(289, 41);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.Size = new System.Drawing.Size(23, 16);
             this.label3.TabIndex = 18;
             this.label3.Text = "ID:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(217, 6);
+            this.label2.Location = new System.Drawing.Point(289, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(92, 16);
             this.label2.TabIndex = 17;
             this.label2.Text = "Selected Item:";
             // 
             // ItemDeleteButton
             // 
-            this.ItemDeleteButton.Location = new System.Drawing.Point(115, 317);
-            this.ItemDeleteButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ItemDeleteButton.Location = new System.Drawing.Point(153, 390);
+            this.ItemDeleteButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ItemDeleteButton.Name = "ItemDeleteButton";
-            this.ItemDeleteButton.Size = new System.Drawing.Size(82, 26);
+            this.ItemDeleteButton.Size = new System.Drawing.Size(109, 32);
             this.ItemDeleteButton.TabIndex = 16;
             this.ItemDeleteButton.Text = "Delete";
             this.ItemDeleteButton.UseVisualStyleBackColor = true;
             // 
             // ItemAddButtom
             // 
-            this.ItemAddButtom.Location = new System.Drawing.Point(14, 317);
-            this.ItemAddButtom.Margin = new System.Windows.Forms.Padding(2);
+            this.ItemAddButtom.Location = new System.Drawing.Point(19, 390);
+            this.ItemAddButtom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ItemAddButtom.Name = "ItemAddButtom";
-            this.ItemAddButtom.Size = new System.Drawing.Size(82, 26);
+            this.ItemAddButtom.Size = new System.Drawing.Size(109, 32);
             this.ItemAddButtom.TabIndex = 15;
             this.ItemAddButtom.Text = "Add";
             this.ItemAddButtom.UseVisualStyleBackColor = true;
+            this.ItemAddButtom.Click += new System.EventHandler(this.ItemAddButtom_Click);
             // 
             // ItemsListBox
             // 
             this.ItemsListBox.FormattingEnabled = true;
-            this.ItemsListBox.Location = new System.Drawing.Point(8, 23);
-            this.ItemsListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.ItemsListBox.ItemHeight = 16;
+            this.ItemsListBox.Location = new System.Drawing.Point(11, 28);
+            this.ItemsListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ItemsListBox.Name = "ItemsListBox";
-            this.ItemsListBox.Size = new System.Drawing.Size(194, 290);
+            this.ItemsListBox.Size = new System.Drawing.Size(257, 356);
             this.ItemsListBox.TabIndex = 14;
             // 
             // Label1
             // 
             this.Label1.AutoSize = true;
-            this.Label1.Location = new System.Drawing.Point(6, 6);
-            this.Label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Label1.Location = new System.Drawing.Point(8, 7);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(35, 13);
+            this.Label1.Size = new System.Drawing.Size(42, 16);
             this.Label1.TabIndex = 13;
             this.Label1.Text = "Items:";
             // 
             // ItemsTabControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.ItemInfoTextBox);
             this.Controls.Add(this.label6);
@@ -174,8 +187,9 @@
             this.Controls.Add(this.ItemAddButtom);
             this.Controls.Add(this.ItemsListBox);
             this.Controls.Add(this.Label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ItemsTabControl";
-            this.Size = new System.Drawing.Size(563, 352);
+            this.Size = new System.Drawing.Size(751, 433);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ObjectOrientedPractics/View/Tabs/ItemsTabControl.cs
+++ ObjectOrientedPractics/View/Tabs/ItemsTabControl.cs
@@ -7,14 +7,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ObjectOrientedPractics.Model;
 
 namespace ObjectOrientedPractics.View.Tabs
 {
     public partial class ItemsTabControl : UserControl
     {
+        /// <summary>
+        /// Коллекция товаров.
+        /// </summary>
+        private List<Item> _items = new List<Item>();
+
+        /// <summary>
+        /// Выбранный студент.
+        /// </summary>
+        private Item _currentItem;
+
+        /// <summary>
+        /// Цвет поля с ошибкой.
+        /// </summary>
+        private readonly Color _errorColor = Color.LightPink;
+
+        /// <summary>
+        /// Цвет поля без ошибок.
+        /// </summary>
+        private readonly Color _normalColor = Color.White;
+
         public ItemsTabControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Возвращает форматированный текст для вывода в List Box. 
+        /// </summary>
+        /// <param name="item"> Товар для форматирования.</param>
+        /// <returns>Возвращает форматированный текст.</returns>
+        private string FormattedText(Item item)
+        {
+            return $"{item.Id} " + $": {item.Name}";
+        }
+
+        /// <summary>
+        ///Очищает информацию о товаре.
+        /// </summary>
+        private void ClearItemInfo()
+        {
+            ItemsListBox.Items.Clear();
+            ItemIDTextBox.Clear();
+            ItemNameTextBox.Clear();
+            ItemInfoTextBox.Clear();
+        }
+
+        /// <summary>
+        /// Обновляет информацию о товаре.
+        /// </summary>
+        /// <param name="student">Товар для обновления информации.</param>
+        private void UpdateStudentInfo(Item item)
+        {
+            int indexSelectedStudent = ItemsListBox.SelectedIndex;
+
+            if (indexSelectedStudent == -1) return;
+
+            ItemsListBox.Items[indexSelectedStudent] = FormattedText(item);
+        }
+
+        private void ItemNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ItemsListBox.SelectedIndex == -1) return;
+
+            try
+            {
+                _currentItem.Name = ItemNameTextBox.Text;
+                UpdateStudentInfo(_currentItem);
+            }
+            catch
+            {
+                ItemNameTextBox.BackColor = _errorColor;
+                return;
+            }
+
+            ItemNameTextBox.BackColor = _normalColor;
+        }
+
+        private void ItemCostTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ItemsListBox.SelectedIndex == -1) return;
+
+            try
+            {
+                _currentItem.Cost = Convert.ToDouble(ItemCostTextBox.Text);
+                UpdateStudentInfo(_currentItem);
+            }
+            catch
+            {
+                ItemCostTextBox.BackColor = _errorColor;
+                return;
+            }
+
+            ItemCostTextBox.BackColor = _normalColor;
+        }
+
+        private void ItemInfoTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (ItemsListBox.SelectedIndex == -1) return;
+
+            try
+            {
+                _currentItem.Info = ItemInfoTextBox.Text;
+                UpdateStudentInfo(_currentItem);
+            }
+            catch
+            {
+                ItemInfoTextBox.BackColor = _errorColor;
+                return;
+            }
+
+            ItemInfoTextBox.BackColor = _normalColor;
+        }
+
+        private void ItemAddButtom_Click(object sender, EventArgs e)
+        {
+            _currentItem = new Item(ItemNameTextBox.Text, ItemInfoTextBox.Text, Convert.ToDouble(ItemCostTextBox.Text));
+            _items.Add(_currentItem);
+            ItemsListBox.Items.Add(FormattedText(_currentItem));
         }
     }
 }
